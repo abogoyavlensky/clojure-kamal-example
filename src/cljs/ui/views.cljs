d653374f4fde33bddc5e828874d9c8cddfa10b5a
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2024-05-21 15:59:06 +0100

    Remove hero section form the html page

--- src/cljs/ui/views.cljs
+++ src/cljs/ui/views.cljs
@@ -17,26 +17,20 @@
   "Render home page."
   [_]
   (let [movies @(re-frame/subscribe [::subs/movies-list-items])]
-    [:section.bg-gray-50
-     [:div.mx-auto.max-w-screen-xl.px-4.py-32.lg:flex.lg:h-screen.lg:items-center
-      [:div.mx-auto.max-w-xl.text-center
-       [:h1.text-3xl.font-extrabold.sm:text-5xl "Welcome to your"
-        [:strong.font-extrabold.text-emerald-700.sm:block "Clojure full-stack application!"]]
-       [:p.mt-4.sm:text-xl/relaxed "Get started by editing `src/cljs/ui/views.cljs`."]
-       ; TODO: keep just title and list of movies
-       [:div.mt-8.flex.flex-wrap.gap-4
-        [:h2.text-3xl "Movies"]
-        [:div.overflow-x-auto
-         [:table.min-w-full.divide-y-2.divide-gray-200.bg-white.text-sm
-          [:thead.ltr:text-left.rtl:text-right
+    [:div.mx-auto.max-w-xl.text-center
+     [:div.mt-8.flex.flex-wrap.gap-4
+      [:h2.text-3xl "Movies"]
+      [:div.overflow-x-auto
+       [:table.min-w-full.divide-y-2.divide-gray-200.bg-white.text-sm
+        [:thead.ltr:text-left.rtl:text-right
+         [:tr
+          [:th.whitespace-nowrap.px-4.py-2.font-medium.text-gray-900 "Title"]
+          [:th.whitespace-nowrap.px-4.py-2.font-medium.text-gray-900 "Year"]
+          [:th.whitespace-nowrap.px-4.py-2.font-medium.text-gray-900 "Director"]]]
+        [:tbody.divide-y.divide-gray-200
+         (for [item movies]
+           ^{:key (:id item)}
            [:tr
-            [:th.whitespace-nowrap.px-4.py-2.font-medium.text-gray-900 "Title"]
-            [:th.whitespace-nowrap.px-4.py-2.font-medium.text-gray-900 "Year"]
-            [:th.whitespace-nowrap.px-4.py-2.font-medium.text-gray-900 "Director"]]]
-          [:tbody.divide-y.divide-gray-200
-           (for [item movies]
-             ^{:key (:id item)}
-             [:tr
-              [:td.whitespace-nowrap.px-4.py-2.font-medium.text-gray-900 (:title item)]
-              [:td.whitespace-nowrap.px-4.py-2.text-gray-700 (:year item)]
-              [:td.whitespace-nowrap.px-4.py-2.text-gray-700 (:director item)]])]]]]]]]))
+            [:td.whitespace-nowrap.px-4.py-2.font-medium.text-gray-900 (:title item)]
+            [:td.whitespace-nowrap.px-4.py-2.text-gray-700 (:year item)]
+            [:td.whitespace-nowrap.px-4.py-2.text-gray-700 (:director item)]])]]]]]))
