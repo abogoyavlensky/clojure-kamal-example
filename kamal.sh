329e85b3673c251b4bbaa44ac4c6c036c5a6251e
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2024-05-22 16:54:43 +0100

    Update kamal script with linux and macos command versions

--- kamal.sh
+++ kamal.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+OS=$(uname)
 KAMAL_VERSION=v1.5.2
-docker run -it --rm -v "${PWD}:/workdir" -v "/run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock" -e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/basecamp/kamal:${KAMAL_VERSION} "$@"
+
+if [[ "$OS" == "Linux" ]]; then
+    docker run -it --rm -v "${PWD}:/workdir" -v "${SSH_AUTH_SOCK}:/ssh-agent" -v /var/run/docker.sock:/var/run/docker.sock -e "SSH_AUTH_SOCK=/ssh-agent" ghcr.io/basecamp/kamal:${KAMAL_VERSION} "$@"
+elif [[ "$OS" == "Darwin" ]]; then
+    docker run -it --rm -v "${PWD}:/workdir" -v "/run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock" -e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/basecamp/kamal:${KAMAL_VERSION} "$@"
+else
+    echo "Unsupported OS"
+fi
